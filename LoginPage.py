f569228f242a308dad628b136c1021d4d1be3112
Jennifer Li <jennifer.lihuiyan@gmail.com>
2023-09-29 10:34:29 -0400

    Create LoginPage

---
+++ LoginPage.py
@@ -0,0 +1,10 @@
+from tkinter import *
+from tkinter import messagebox
+
+root=Tk()
+root.title('Login')
+root.geometry('925x500+300+200')
+root.configure(bg="#fff")
+root.resizable(False, False)
+
+root.mainloop()
