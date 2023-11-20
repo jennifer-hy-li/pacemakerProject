5df9031d153ba2e10f03cf4c1167e3eb08dc49b6
Jayden Hooper <jaydenkylehooper@gmail.com>
2023-11-20 16:05:28 -0500

    confirmed serial comms working properly

--- SerialCommunication.py
+++ SerialCommunication.py
@@ -1,40 +1,34 @@
 import serial
 import bitstring
 import struct
+import time
 
 def main(port='COM6', baudrate=115200, timeout=0):
     # write an array of data
     with serial.Serial(port, baudrate, timeout=timeout) as ser:
-        print("Sending:", params_as_bytes())
-        print("Writing", ser.write(params_as_bytes()), "bytes")
-        #  read until received 31 bytes
-        print("Reading", ser.read_until(size=35), "bytes")
+        print("Sending:", set_parameters())
+        print("Writing", ser.write(set_parameters()), "bytes")
+        # sleep 10 seconds
+        time.sleep(10)
+        print("Writing", ser.write(set_parameters(LRL = 10)), "bytes")
+        time.sleep(10)
+        print("Writing", ser.write(set_parameters(MODE = 4)), "bytes")
+        time.sleep(10)
+        print("Writing", ser.write(set_parameters(MODE = 1)), "bytes")
+        time.sleep(10)
+        print("Writing", ser.write(set_parameters(MODE = 2)), "bytes")
 
-
-
-def params_as_bytes():
-    params: bytearray  = []
+def set_parameters(MODE = 3, LRL = 60, URL = 120, ARP_DELAY = 200, 
+                   ATR_AMP = 3.5, VENT_AMP = 3.5, VRP_DELAY = 200, 
+                   ATR_PW = 10, VENT_PW = 10, ATR_THRESHOLD = 1.8, 
+                   VENT_THRESHOLD = 2.2):
+    """Sets the parameters for the pacemaker"""
     STANDARD: int = 22
     GIVE_PARAMS: int = 18
     TEST_PARAMS: int = 34
-    MODE: int = 1  # VOO
-    LRL: int = 50
-    URL: int = 120
-    ARP_DELAY: int = 200
-    ATR_AMP: float = 3.5
-    VENT_AMP: float = 2.0
-    VRP_DELAY: int = 200
-    ATR_PW: int = 10
-    VENT_PW: int = 2
-    ATR_THRESHOLD: float = 1.8
-    VENT_THRESHOLD: float = 2.2
-    # convert to bytes and append to params
-
-
     parameters = struct.pack('<BBBHHHffHHHff', STANDARD, GIVE_PARAMS, MODE, LRL, URL, 
                              ARP_DELAY, ATR_AMP, VENT_AMP, VRP_DELAY, ATR_PW, VENT_PW,
                               ATR_THRESHOLD, VENT_THRESHOLD)
-    print(parameters)
     return parameters
 
 def convert_to_bytes(num, bytes = 8):
@@ -45,4 +39,3 @@ def convert_to_bytes(num, bytes = 8):
     
 if __name__ == '__main__':
     main()
-    # print(params_as_bytes())
