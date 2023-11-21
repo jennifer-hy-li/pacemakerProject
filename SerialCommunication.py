47e98aa3664409651d11e30bfaa4d9eb805d25df
Jayden Hooper <jaydenkylehooper@gmail.com>
2023-11-21 17:09:20 -0500

    Update SerialCommunication.py

--- SerialCommunication.py
+++ SerialCommunication.py
@@ -1,28 +1,25 @@
 import serial
-import bitstring
 import struct
-import time
 
-def main(port='COM6', baudrate=115200, timeout=0):
+def write(port='COM6', baudrate=115200, timeout=0):
     # write an array of data
     with serial.Serial(port, baudrate, timeout=timeout) as ser:
-        print("Sending:", set_parameters())
-        print("Writing", ser.write(set_parameters()), "bytes")
-        # sleep 10 seconds
-        time.sleep(10)
-        print("Writing", ser.write(set_parameters(LRL = 10)), "bytes")
-        time.sleep(10)
         print("Writing", ser.write(set_parameters(MODE = 4)), "bytes")
-        time.sleep(10)
-        print("Writing", ser.write(set_parameters(MODE = 1)), "bytes")
-        time.sleep(10)
-        print("Writing", ser.write(set_parameters(MODE = 2)), "bytes")
-        print("Reading", read_egram_data(), "bytes")
 
-def set_parameters(MODE = 3, LRL = 60, URL = 120, ARP_DELAY = 1, 
-                   ATR_AMP = 3.5, VENT_AMP = 3.5, VRP_DELAY = 1, 
-                   ATR_PW = 10, VENT_PW = 10, ATR_THRESHOLD = 1.8, 
-                   VENT_THRESHOLD = 2.2):
+def read(port='COM6', baudrate=115200, timeout=0):
+    with serial.Serial(port, baudrate, timeout=timeout) as ser:
+        print("Sending", ser.write(send_read_parameters()), "bytes of read parameters")
+        line = ser.read(35)
+        if line:
+            # line = struct.unpack('<BBBHHHffHHHff', line)   # <-- uncomment when data has been received
+            print("Data received:", line)
+        else:
+            print("No data received:", line)
+
+def set_parameters(MODE = 3, LRL = 60, URL = 120, ARP_DELAY = 200, 
+                   ATR_AMP = 3.5, VENT_AMP = 3.5, VRP_DELAY = 200, 
+                   ATR_PW = 10, VENT_PW = 10, ATR_THRESHOLD = 80, 
+                   VENT_THRESHOLD = 80):
     """Sets the parameters for the pacemaker"""
     STANDARD: int = 22
     GIVE_PARAMS: int = 18
@@ -31,18 +28,13 @@ def set_parameters(MODE = 3, LRL = 60, URL = 120, ARP_DELAY = 1,
                               ATR_THRESHOLD, VENT_THRESHOLD)
     return parameters
 
-def read_egram_data():
+def send_read_parameters():
     """Reads the egram data from the pacemaker"""
     STANDARD: int = 22
     READ_EGRAM: int = 34
     data = struct.pack('<BB', STANDARD, READ_EGRAM)
+    # data = struct.pack('<BBBHHHffHHHff', STANDARD, READ_EGRAM, 1, 60, 120, 1, 3.5, 3.5, 1, 10, 10, 1.8, 2.2) 
     return data
 
-def convert_to_bytes(num, bytes = 8):
-    """Converts a number to a bitstring of length bytes*8"""
-    if bytes == 8:
-        return bitstring.BitArray(float=num, length=bytes*8).bytes
-    return bitstring.BitArray(int=num, length=bytes*8).bytes
-    
 if __name__ == '__main__':
-    main()
+    write()
