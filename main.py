191f51212d57fa3f7966449960490bac5a62c6be
Jayden Hooper <jaydenkylehooper@gmail.com>
2023-11-30 11:36:08 -0500

    removed unnecessary code

--- main.py
+++ main.py
@@ -7,8 +7,6 @@ def main():
     db = PacemakerDatabase.get_instance()
     if not db.table_exists('account'):
         db.create_and_populate()
-    x = db.get_all_account_parameters('123')
-    print(x)
     MainWindow(tk.Tk())
     tk.mainloop()
 
