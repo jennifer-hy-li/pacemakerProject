afbdeffa6af8ece42b9f6d46789cddbbcc518240
Jayden Hooper <jaydenkylehooper@gmail.com>
2023-11-29 21:09:58 -0500

    cleaned up communication code

--- SerialCommunication.py
+++ SerialCommunication.py
@@ -4,25 +4,17 @@ import struct
 def write(port='COM6', baudrate=115200, timeout=0):
     # write an array of data
     with serial.Serial(port, baudrate, timeout=timeout) as ser:
-        print("Writing", ser.write(set_parameters(RECEIVE = True, MODE = 3)), "bytes")
-        while True:
-            bytes_to_read = ser.in_waiting
-            print(bytes_to_read)
-            if bytes_to_read == 29:
-                read_data = ser.read(bytes_to_read)
-                print(struct.unpack('<HHBHHHffffH', read_data))
-                break
-        
+        print("Writing", ser.write(set_parameters(RECEIVE = False, MODE = 3)), "bytes")        
 
 def read(port='COM6', baudrate=115200, timeout=0):
     with serial.Serial(port, baudrate, timeout=timeout) as ser:
         print("Sending", ser.write(set_parameters(RECEIVE = True)), "bytes of read parameters")
-        line = ser.readline()
-        if line:
-            # line = struct.unpack('<BBBHHHffHHHff', line)   # <-- uncomment when data has been received
-            print("Data received:", line)
-        else:
-            print("No data received:", line)
+        while True:
+            if ser.in_waiting == 29:
+                read_data = ser.read(ser.in_waiting)
+                unpacked_data = struct.unpack('<HHBHHHffffH', read_data)
+                break
+        print("Successfully read data:", struct.unpack('<HHBHHHffffH', read_data))
 
 def set_parameters(RECEIVE = True, MODE = 3, LRL = 60, URL = 120, ARP_DELAY = 200, 
                    ATR_AMP = 3.5, VENT_AMP = 3.5, VRP_DELAY = 200, 
@@ -37,4 +29,4 @@ def set_parameters(RECEIVE = True, MODE = 3, LRL = 60, URL = 120, ARP_DELAY = 20
     return parameters
 
 if __name__ == '__main__':
-    write()
+    read()
