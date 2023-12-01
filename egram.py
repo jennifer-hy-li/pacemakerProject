7e0c18992832c979fa38d8c4acc3be9f29886610
Jayden Hooper <jaydenkylehooper@gmail.com>
2023-11-30 20:15:06 -0500

    Update egram.py

--- egram.py
+++ egram.py
@@ -5,80 +5,63 @@ import matplotlib.animation as animation
 import SerialCommunication as sc
 
 class egram():
-    __time: deque = None
-    __atr_timeline: deque = None
-    __vent_timeline: deque = None
 
     def __init__(self):
-        self.__time = deque()
-        self.__atr_timeline = deque()
-        self.__vent_timeline = deque()
-    
-    def append_signals(self, atr_milliVolts: float, vent_milliVolts: float):
-        self.__time.append(datetime.now())
-        self.__atr_timeline.append(atr_milliVolts)
-        self.__vent_timeline.append(vent_milliVolts)
-
-    # def pop(self):
-    #     return self.__timeline.pop()
-    
-    # def popleft(self):
-    #     return self.__timeline.popleft()
-    
-    # def length(self):
-    #     return len(self.__timeline)
-    
-    # def isEmpty(self):
-    #     return len(self.__timeline) == 0
-    
-    def display(self):
-        # display 2 graphs, one for atrium and one for ventricle in subplots
-        plt.subplot(2,1,1)
-        plt.subplots_adjust(hspace=0.5)
-        plt.plot(self.__time, self.__atr_timeline)
-        plt.title('Atrium')
-        plt.xlabel('Time')
-        plt.ylabel('Voltage')
-        plt.subplot(2,1,2)
-        plt.plot(self.__time, self.__vent_timeline)
-        plt.title('Ventricle')
-        plt.xlabel('Time')
-        plt.ylabel('Voltage')
-        plt.show()
-
-    def init_animation(self):
         self.fig = plt.figure()
         self.ax = self.fig.add_subplot(1,1,1)
-        self.xs = []
-        self.ys = []
+        self.signals = []
+        self.timestamps = []
 
-    def animate_atrium(self, i):
+    def animate_signal_helper(self, i, signal_index: int):
+        # Read pacemaker signals
         signals = sc.read()
-        atr_signal = signals[0]
-        vent_signal = signals[1]
+        signal = signals[signal_index]
 
         # Add x and y to lists
-        self.xs.append(datetime.now())
-        self.ys.append(atr_signal)
-
-        # Limit x and y lists to 20 items
-        self.xs = self.xs[-100:]
-        self.ys = self.ys[-100:]
+        self.signals.append(signal)
+        self.timestamps.append(datetime.now())
+        
+        # Limit x and y lists to 100 items
+        self.signals = self.signals[-100:]
+        self.timestamps = self.timestamps[-100:]
 
         # Draw x and y lists
         self.ax.clear()
-        self.ax.plot(self.xs, self.ys)
+        self.ax.plot(self.timestamps, self.signals)
 
         # Format plot
         plt.xticks(rotation=45, ha='right')
         plt.subplots_adjust(bottom=0.30)
-        plt.title('Atrium')
+        if signal_index == 0:
+            plt.title('Atrium')
+        else:
+            plt.title('Ventricle')
         plt.xlabel('Time')
         plt.ylabel('Voltage')
 
+    def animate_atr(self, i):
+        self.animate_signal_helper(i, 0)
+
+    def animate_vent(self, i):
+        self.animate_signal_helper(i, 1)
+
+    def animate_signals(self, i):
+        """Animates both atrium and ventricle signals in subplots,
+        with the atrium on top and the ventricle on the bottom"""
+        fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True)
+        self.ax1 = ax1
+        self.ax2 = ax2
+        self.animate_atr(i)
+        self.animate_vent(i)
+        
+
+
+
     
 e = egram()
-e.init_animation()
-ani = animation.FuncAnimation(e.fig, e.animate_atrium, interval=50)
+# ani = animation.FuncAnimation(e.fig, e.animate_atr, interval=50)
+# plt.show()
+
+ani = animation.FuncAnimation(e.fig, e.animate_signals, interval=50)
 plt.show()
     
