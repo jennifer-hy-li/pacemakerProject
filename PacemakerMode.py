25e22875c5c3dcb596487460ef37a0620c61d61a
Jayden Hooper <jaydenkylehooper@gmail.com>
2023-12-01 09:32:52 -0500

    updated voo

--- PacemakerMode.py
+++ PacemakerMode.py
@@ -94,11 +94,7 @@ class AOO(ParameterProcess,tk.Frame):
             else: 
                 value.set(default_val)
                 self.parameter_tuples.append((param[1], value)) # (parameter_name, value_var)
-
-            # SELECT parameter FROM accountparameters WHERE '{getUser()}' = username AND 'AOO' = mode AND '{param[1]}' = parameter
-
-            # print(param[1])
-            
+        
             
 
             label = tk.Label(self, text=f" {param[1]}", font=('Arial', 12))
@@ -138,40 +134,91 @@ class AOO(ParameterProcess,tk.Frame):
         ParameterProcess.process_parameter(parameters_to_save, "AOO")
         print("Parameters saved.")
 
-class VOO(tk.Frame):
+
+class VOO(ParameterProcess,tk.Frame):
+    
     def __init__(self,parent):
         super().__init__(parent)
-        tk.Label(self, text="MODE: VOO", font = ('Arial',20)).grid(row = 1, column=0, padx=(70,0))
-        tk.Label(self, text="Ventrical Paced | No chamber sensed | No response to sensing ", font = ('Arial',13)).grid(row=2,column=0,padx=(70,0),pady=(0,10))
         
-        # Create Entry widgets for parameters
-        self.param_entries = {}
-        self.parameter_values = {}
+        tk.Label(self, text="Mode: VOO", font = ('Arial',23)).grid(row = 1, column=0, padx=(50,0))
+        tk.Label(self, text="Atrium Paced | No chamber sensed | No response to sensing ", 
+                 font = ('Arial',12)).grid(row=2,column=0,padx=(50,0),pady=(0,10))
+        
+        """Check if the user has any saved parameters
+       If so, use those values as the default values
+       Otherwise, use the default values from the modeparam table"""
 
-        parameters = db.get_parameters(mode ="VOO")
-        parameter_tuples: list(tuple) = []
+        parameters = db.get_parameters(mode = "VOO")  # 4 parameters assigned to mode VOO
+        saved_value = db.get_parameters(username = getUser(), mode = "VOO")  # length 4 means theres 4 saved parameters for this user for VOO
+
+        # SETS AND SAVES VALUES TO TEXT FIELDS
+        
+        self.parameter_tuples: list(tuple) = []
+        parameters_max = []
+        parameters_min = []
+        parameters_increment =[]
+        i=0
+        row = 5
         for param in parameters:
-            value = tk.StringVar()
-            print(param)
-            parameter_tuples.append((param[1], value))
-        
-        row = 5  # Starting row for parameters
-        for parameter_name, value_var in parameter_tuples:
-            label = tk.Label(self, text=f" {parameter_name}", font=('Arial', 12))
-            label.grid(row=row, column=0, sticky="w", padx=(0, 0))
-            entry = tk.Entry(self, textvariable=value_var)
-            entry.grid(row=row, column=0, padx=(50, 0))  # Use padx to adjust the spacing
-            self.param_entries[parameter_name] = entry
+            
+            #parameters= db.get_modes_from_modeparameters()
+            #print(parameters[i])
+            default_val = parameters[i][3]
+            
+            #print("param max",parameter_max)
+            
+            value = tk.DoubleVar()
+            print("Check if saved value length matches parameter value length", len(saved_value), len(parameters))
+            if len(saved_value) == len(parameters) and len(saved_value) != 0:
+                
+                data = db.get_parameters(username = getUser(), mode = "VOO", parameter = param[1])[0]
 
+                print(data)
+                value.set(data[3])
+                self.parameter_tuples.append((data[0], value)) # (parameter_name, value_var)
+                print(self.parameter_tuples[i][0], self.parameter_tuples[i][1].get())
+            else: 
+                value.set(default_val)
+                self.parameter_tuples.append((param[1], value)) # (parameter_name, value_var)
+        
+            
+
+            label = tk.Label(self, text=f" {param[1]}", font=('Arial', 12))
+            label.grid(row=row, column=0, sticky="w", padx=(0, 0))
+    
+            parameters_increment.append(parameters[i][6])
+            parameters_max.append(parameters[i][5])
+
+            self.increment_button = tk.Button(self, text="+", command = lambda v =value, i = i: self.increment_counter(v, parameters_max[i],self.parameter_tuples[i][0],parameters_increment[i]))
+            self.increment_button.grid(row=row, column=0, padx=(100, 0)) 
+
+            num = tk.Label(self, textvariable=value, font=('Arial', 16))
+            num.grid(row=row, column=0, padx=(20, 20))   # Adjusted column for the label
+
+        
+            parameters_min.append(parameters[i][4])
+            self.decrement_button = tk.Button(self, text="-", command = lambda v = value, i = i,mode="VOO",j=i: self.decrement_counter(v, parameters_min[i],mode,parameters_increment[j]))
+            self.decrement_button.grid(row=row, column=0, padx=(0, 100)) 
             # Add a button for each parameter
-            process_button = tk.Button(self, text=f"Process", command=lambda p=parameter_name, v=value_var,m="VOO": ParameterProcess.process_parameter([(p,v)],m))
+            process_button = tk.Button(self, text=f"Process", command=lambda p=param[1], v=value,m="VOO": ParameterProcess.process_parameter([(p,v)],m))
             process_button.grid(row=row, column=0,padx=(300, 50))  # Place the button in a separate column
             row += 1
+            i+=1
         
+        save_button = tk.Button(self, text="Save", command=self.save_parameters)
+        save_button.grid(row=row, column=0, padx=(300, 50), pady=(20, 0))
+
         tk.Label(self, text="Device:", font=('Arial', 10)).grid(row=30, column=0, padx=(0, 550), pady=(140, 0))
         tk.Label(self, text="[Show status + Device ID]", font=('Arial', 9)).grid(row=31, column=0, padx=(0, 450), pady=(0, 0))
 
         self.pack()
+
+    def save_parameters(self):
+        # Save all processed parameters
+        parameters_to_save = [(param_name, value_var) for param_name, value_var in self.parameter_tuples]
+        print(parameters_to_save)
+        ParameterProcess.process_parameter(parameters_to_save, "VOO")
+        print("Parameters saved.")
 
 
 class AAI (tk.Frame):
